aedabd1b504aaabc659e8a8447915b26dd25461d
Raul Villora Valencia <r.vvalencia@alumnos.upm.es>
2019-02-10 13:43:42 +0100

    Change App.js and App.css code

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>Hello</div>
+      <div className="demo-big-content">
+      <Layout>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">Who I am</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">Who I am</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main/>
+        </Content>
+    </Layout>
+</div>
     );
   }
 }
