1142e5ed55521c16d3c2203beefa4ebe49612876
Raul Villora Valencia <r.vvalencia@alumnos.upm.es>
2019-02-12 10:04:06 +0100

    Changed CSS

---
+++ build/precache-manifest.4c1eeac2bab8514eb5b507bb43202b4a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "69e5b1aebd0ae4087271",
+    "url": "/my-portfolio/static/css/main.bcc42750.chunk.css"
+  },
+  {
+    "revision": "69e5b1aebd0ae4087271",
+    "url": "/my-portfolio/static/js/main.69e5b1ae.chunk.js"
+  },
+  {
+    "revision": "63be515cec124203d86f",
+    "url": "/my-portfolio/static/css/1.367f1f7f.chunk.css"
+  },
+  {
+    "revision": "63be515cec124203d86f",
+    "url": "/my-portfolio/static/js/1.63be515c.chunk.js"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "e13a69c1eca2503f6425c67af9f2f67c",
+    "url": "/my-portfolio/static/media/Me.e13a69c1.jpg"
+  },
+  {
+    "revision": "942219802d19cc8ef97836107fbfbcdb",
+    "url": "/my-portfolio/static/media/BigPicture.94221980.jpg"
+  },
+  {
+    "revision": "eed65648482a5021d8a974391b36599c",
+    "url": "/my-portfolio/static/media/Explorer.eed65648.jpg"
+  },
+  {
+    "revision": "e8b86fa45909eac993bdf542c24f2029",
+    "url": "/my-portfolio/static/media/Volunt.e8b86fa4.jpg"
+  },
+  {
+    "revision": "6b1c5f1868e84d4aba3ea77de9ae5522",
+    "url": "/my-portfolio/static/media/Flexible.6b1c5f18.jpg"
+  },
+  {
+    "revision": "681c8367926a4301bcf4d205b06ff673",
+    "url": "/my-portfolio/static/media/me.681c8367.png"
+  },
+  {
+    "revision": "dbcae68de03ee12a9361b21895321183",
+    "url": "/my-portfolio/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3845e0c33c82315be4eaaca10eab834a.js"
+  "/my-portfolio/precache-manifest.4c1eeac2bab8514eb5b507bb43202b4a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/my-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
