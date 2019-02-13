06cb018f94f4a1939ed7a65e2edd440af0411e77
Raul Villora Valencia <r.vvalencia@alumnos.upm.es>
2019-02-13 19:00:29 +0100

    Changed aboutme component

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -53,6 +53,49 @@ export default class About extends Component {
             </div>
             </div>
             <br></br>
+
+            <div className="theContainer">
+            <div className="banner-text-about-me">
+              <h2>I AM SELFLESS</h2>
+              <div className="wprBody">
+                <div className="column" >
+                  <p>I am genuinely concern about other people well-being, and it has been one of the primary reasons which have driven me to work as a volunteer in several occasions for different organizations but all of them with the same simple goal of making this world a better one.
+                    Volunteering has given me the opportunity to explore my limits and go beyond them, to break the shackles of routine life and to boost up my interpersonal skills.</p>
+                </div>
+                <div className="column">
+                  <img
+                    src={Volunt}
+                    alt="Image2"
+                  />
+                </div>
+              </div>
+               
+              <hr />
+            </div>
+            </div>
+            <br></br>
+
+
+            <div className="theContainer">
+            <div className="banner-text-about-me">
+              <h2>I AM FLEXIBLE</h2>
+              <div className="wprBody">
+                <div className="column" >
+                  <p>Nothing lasts forever but the certainty of change. Over the years, I’ve learnt to live with that fact. And not only live with it – I’ve learnt to enjoy it, embrace it, work with it.
+                  Titles have never defined or restricted me. No task is too big or small – as long as the job gets done. What I don’t know, I learn it. I am flexible.</p>
+                </div>
+                <div className="column">
+                  <img
+                    src={Flexible}
+                    alt="Image3"
+                  />
+                </div>
+              </div>
+               
+              <hr />
+            </div>
+            </div>
+            <br></br>
         </Grid>
       </div>
     )
