9e445cc611005b422c29cebcdaf4c66abd0e9fc9
Raul Villora Valencia <r.vvalencia@alumnos.upm.es>
2019-03-14 17:31:47 +0100

    Edited image 2048, changed CSS and proejct component

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
+import {CardText, CardActions, Button} from 'react-mdl';
 import Portfolio from '../images/Portfolio.jpg';
 import Game2048 from '../images/2048.jpg';
 
@@ -12,9 +12,11 @@ class Projects extends Component {
 					<Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
 						<img src={Portfolio} alt="" />
 
-						<CardText style={{ fontFamily: 'Avenir' }}>
-							Raul's portfolio is a website where you can find out who I am and know more about my
-							academic background and my work experience.
+						<CardText>
+							<div className="cardTextProjects">
+								Raul's portfolio is a website where you can find out who I am and know more about my
+								academic background and my work experience.
+							</div>
 						</CardText>
 						<CardActions border>
 							<Button colored>
@@ -34,7 +36,9 @@ class Projects extends Component {
 				<div className="projects-grid">
 					<Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
 						<img src={Game2048} alt="" />
-						<CardText style={{ fontFamily: 'Avenir' }}>React version of the game 2048</CardText>
+						<CardText>
+							<div className="cardTextProjects">React version of the game 2048</div>
+						</CardText>
 						<CardActions border>
 							<Button colored>
 								<a href="https://github.com/raulvillora/2048/" style={{ fontFamily: 'Avenir' }}>
